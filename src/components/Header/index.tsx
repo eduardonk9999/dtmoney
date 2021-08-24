29d27759b011b02c4b9ba350464f423e8721bdf9
eduardonk9999 <thiago.novak7@gmail.com>
2021-08-24 12:18:56 -0300

    Add, table

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export function Header() {
      <Content>
       <img src={logoImg} alt="dt money" />
         <button type="button">
-          Nova transação
+          Nova transação!
         </button>
       </Content>
     </ Container>
